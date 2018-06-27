5b6977a5a4d19b75da30d3124904820b991bac23
Furquims <40636375+Furquims@users.noreply.github.com>
2018-06-27 14:13:55 -0300

    Update Oled.c
    Adicionado o Print do botão Select

--- Oled.c
+++ Oled.c
@@ -1,141 +1,281 @@
 //Projeto Oled
+
 //Bruno B. Furquim
 
 
+
+
+
 /****************************************************
+
 *                  FUNCIONOU                        *
+
 ****************************************************/
 
 
+
+
+
 #include "U8glib.h"
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);	// Display which does not send AC
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);  // Display which does not send AC
+
+
 
 #define KEY_NONE 0
+
 #define KEY_PREV 1
+
 #define KEY_NEXT 2
+
 #define KEY_SELECT 3
+
 #define KEY_BACK 4
 
+
+
 uint8_t uiKeyPrev = 7;
+
 uint8_t uiKeyNext = 3;
+
 uint8_t uiKeySelect = 2;
+
 uint8_t uiKeyBack = 8;
 
+
+
 uint8_t uiKeyCodeFirst = KEY_NONE;
+
 uint8_t uiKeyCodeSecond = KEY_NONE;
+
 uint8_t uiKeyCode = KEY_NONE;
 
 
+
+
+
 void uiSetup(void) {
+
   // configure input keys 
+
   
+
   pinMode(uiKeyPrev, INPUT_PULLUP);           // set pin to input with pullup
+
   pinMode(uiKeyNext, INPUT_PULLUP);           // set pin to input with pullup
+
   pinMode(uiKeySelect, INPUT_PULLUP);           // set pin to input with pullup
+
   pinMode(uiKeyBack, INPUT_PULLUP);           // set pin to input with pullup
+
 }
 
+
+
 void uiStep(void) {
+
   uiKeyCodeSecond = uiKeyCodeFirst;
+
   if ( digitalRead(uiKeyPrev) == LOW )
+
     uiKeyCodeFirst = KEY_PREV;
+
   else if ( digitalRead(uiKeyNext) == LOW )
+
     uiKeyCodeFirst = KEY_NEXT;
+
   else if ( digitalRead(uiKeySelect) == LOW )
-    uiKeyCodeFirst = KEY_SELECT;
-  else if ( digitalRead(uiKeyBack) == LOW )
-    uiKeyCodeFirst = KEY_BACK;
-  else 
-    uiKeyCodeFirst = KEY_NONE;
+  {
+  uiKeyCodeFirst = KEY_SELECT;
+    Serial.println(digitalRead(uiKeySelect));
+    delay(100);
+  }
   
+  else if ( digitalRead(uiKeyBack) == LOW )
+
+    uiKeyCodeFirst = KEY_BACK;
+
+  else 
+
+    uiKeyCodeFirst = KEY_NONE;
+
+  
+
   if ( uiKeyCodeSecond == uiKeyCodeFirst )
+
     uiKeyCode = uiKeyCodeFirst;
+
   else
+
     uiKeyCode = KEY_NONE;
+
 }
+
+
+
 
 
 #define MENU_ITEMS 4
+
 #define N_MENUS 2
+
 const char *menu_strings[N_MENUS][MENU_ITEMS] = {{"uno", "2", "tres", "quatorze"}, {"First Line", "Second Item", "3333333", "abcdefg"}};
 
+
+
 uint8_t menu_current = 0;
+
 uint8_t menu_redraw_required = 0;
+
 uint8_t last_key_code = KEY_NONE;
 
+
+
 // Normalmente usa-se int, porque as funções deveriam retornar 1 em caso de sucesso ou 0 em caso de falha.
+
 int drawMenu(int n) {
+
   uint8_t i, h;
+
   u8g_uint_t w, d;
 
+
+
   u8g.setFont(u8g_font_6x13);
+
   u8g.setFontRefHeightText();
+
   u8g.setFontPosTop();
+
   
+
   h = u8g.getFontAscent()-u8g.getFontDescent();
+
   w = u8g.getWidth();
+
   for( i = 0; i < MENU_ITEMS; i++ ) {
+
     d = (w-u8g.getStrWidth(menu_strings[n][i]))/2;
+
     u8g.setDefaultForegroundColor();
+
     if ( i == menu_current ) {
+
       u8g.drawBox(0, i*h+1, w, h);
+
       u8g.setDefaultBackgroundColor();
+
     }
+
     u8g.drawStr(d, i*h, menu_strings[n][i]);
+
   }
   return 1;
+
 }
+
+
 
 /*
+
 Coloquei um int e retorno pra avisar a troca de tela usando a tecla SELECT, isso será tratado no loop.
+
 */
 
+
+
 int updateMenu(void) {
+
   if ( uiKeyCode != KEY_NONE && last_key_code == uiKeyCode ) {
+
     return;
+
   }
+
   last_key_code = uiKeyCode;
+
   
+
   switch ( uiKeyCode ) {
+
     case KEY_NEXT:
+
       menu_current++;
+
       if ( menu_current >= MENU_ITEMS )
+
         menu_current = 0;
+
       menu_redraw_required = 1;
+
       break;
+
     case KEY_PREV:
+
       if ( menu_current == 0 )
+
         menu_current = MENU_ITEMS;
+
       menu_current--;
+
       menu_redraw_required = 1;
+
       break;
+
     case KEY_SELECT:
+
       return 1;
+
   }
+
   return 0;
+
 }
 
-void setup() {
-  // rotate screen, if required
-  // u8g.setRot180();
-  
+
+
+
+void setup()
+{
+
+  Serial.begin(9600);
+
   uiSetup();                                // setup key detection and debounce algorithm
+
   menu_redraw_required = 1;     // force initial redraw
+
 }
+
+
 
 void loop() {  
+
   int n_menu = 0;
+
   uiStep();                                     // check for key press
+
     
+
   if (  menu_redraw_required != 0 ) {
+
     u8g.firstPage();
+
     do  {
+
       drawMenu(n_menu); // Passando qual o menu será mostrado.
+
     } while( u8g.nextPage() );
+
     menu_redraw_required = 0;
+
   }
 
+
+
   if ( updateMenu() ){                            // update menu bar
+
     n_menu = (n_menu + 1) % N_MENUS;
+
   }
+
 }
