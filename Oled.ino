ec78eb21a295364ff6ea49a4abc2eedea902ae54
Furquims <40636375+Furquims@users.noreply.github.com>
2018-06-29 22:33:28 -0300

    Implementação de multiplos menus.

--- Oled.ino
+++ Oled.ino
@@ -1,14 +1,10 @@
 //Projeto Oled
 //Bruno B. Furquim
 
-
-/****************************************************
-*                  FUNCIONOU                        *
-****************************************************/
-
-
 #include "U8glib.h"
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);	// Display which does not send AC
+#include <stdio.h>
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);  // Display which does not send AC
 
 #define KEY_NONE 0
 #define KEY_PREV 1
@@ -25,28 +21,37 @@ uint8_t uiKeyCodeFirst = KEY_NONE;
 uint8_t uiKeyCodeSecond = KEY_NONE;
 uint8_t uiKeyCode = KEY_NONE;
 
-
 void uiSetup(void) {
   // configure input keys 
-  
   pinMode(uiKeyPrev, INPUT_PULLUP);           // set pin to input with pullup
   pinMode(uiKeyNext, INPUT_PULLUP);           // set pin to input with pullup
-  pinMode(uiKeySelect, INPUT_PULLUP);           // set pin to input with pullup
+  pinMode(uiKeySelect, INPUT_PULLUP);         // set pin to input with pullup
   pinMode(uiKeyBack, INPUT_PULLUP);           // set pin to input with pullup
 }
 
 void uiStep(void) {
   uiKeyCodeSecond = uiKeyCodeFirst;
-  if ( digitalRead(uiKeyPrev) == LOW )
+  if ( digitalRead(uiKeyPrev) == LOW ){
+    Serial.println("Prev");
+    delay(20);
     uiKeyCodeFirst = KEY_PREV;
-  else if ( digitalRead(uiKeyNext) == LOW )
+  }
+  else if ( digitalRead(uiKeyNext) == LOW ){
     uiKeyCodeFirst = KEY_NEXT;
-  else if ( digitalRead(uiKeySelect) == LOW )
-    uiKeyCodeFirst = KEY_SELECT;
-  else if ( digitalRead(uiKeyBack) == LOW )
+    delay(20);
+    Serial.println("Next");
+  }
+  else if ( digitalRead(uiKeySelect) == LOW ){
+    Serial.println("Select");
+    delay(20);
+    uiKeyCodeFirst = KEY_SELECT; 
+  }
+  else if ( digitalRead(uiKeyBack) == LOW ){
+    Serial.println("Back");
+    delay(20);
     uiKeyCodeFirst = KEY_BACK;
-  else 
-    uiKeyCodeFirst = KEY_NONE;
+  }
+  else uiKeyCodeFirst = KEY_NONE;
   
   if ( uiKeyCodeSecond == uiKeyCodeFirst )
     uiKeyCode = uiKeyCodeFirst;
@@ -54,11 +59,10 @@ void uiStep(void) {
     uiKeyCode = KEY_NONE;
 }
 
-
 #define MENU_ITEMS 4
-#define N_MENUS 2
-const char *menu_strings[N_MENUS][MENU_ITEMS] = {{"uno", "2", "tres", "quatorze"}, {"First Line", "Second Item", "3333333", "abcdefg"}};
+#define N_MENUS 3
 
+const char *menu_strings[N_MENUS][MENU_ITEMS] = {{"uno", "2", "tres", "quatorze"}, {"First Line", "Second Item", "3333333", "abcdefg"}, {"aaaaa", "bbbbbb", "cccccc", "hauhauhahauha"}};
 uint8_t menu_current = 0;
 uint8_t menu_redraw_required = 0;
 uint8_t last_key_code = KEY_NONE;
@@ -67,11 +71,9 @@ uint8_t last_key_code = KEY_NONE;
 int drawMenu(int n) {
   uint8_t i, h;
   u8g_uint_t w, d;
-
   u8g.setFont(u8g_font_6x13);
   u8g.setFontRefHeightText();
   u8g.setFontPosTop();
-  
   h = u8g.getFontAscent()-u8g.getFontDescent();
   w = u8g.getWidth();
   for( i = 0; i < MENU_ITEMS; i++ ) {
@@ -86,16 +88,13 @@ int drawMenu(int n) {
   return 1;
 }
 
-/*
-Coloquei um int e retorno pra avisar a troca de tela usando a tecla SELECT, isso será tratado no loop.
-*/
+/* Coloquei um int e retorno pra avisar a troca de tela usando a tecla SELECT, isso será tratado no loop. */
 
 int updateMenu(void) {
   if ( uiKeyCode != KEY_NONE && last_key_code == uiKeyCode ) {
-    return;
+    return 0;
   }
   last_key_code = uiKeyCode;
-  
   switch ( uiKeyCode ) {
     case KEY_NEXT:
       menu_current++;
@@ -110,23 +109,26 @@ int updateMenu(void) {
       menu_redraw_required = 1;
       break;
     case KEY_SELECT:
+      menu_redraw_required = 1;
       return 1;
   }
   return 0;
 }
 
+int n_menu;
+
 void setup() {
   // rotate screen, if required
   // u8g.setRot180();
-  
-  uiSetup();                                // setup key detection and debounce algorithm
+  uiSetup();                    // setup key detection and debounce algorithm
   menu_redraw_required = 1;     // force initial redraw
+  n_menu = 0;
 }
 
 void loop() {  
-  int n_menu = 0;
-  uiStep();                                     // check for key press
-    
+  Serial.begin(9600);
+  delay(20);
+  uiStep();               // check for key press
   if (  menu_redraw_required != 0 ) {
     u8g.firstPage();
     do  {
@@ -134,8 +136,8 @@ void loop() {
     } while( u8g.nextPage() );
     menu_redraw_required = 0;
   }
-
   if ( updateMenu() ){                            // update menu bar
     n_menu = (n_menu + 1) % N_MENUS;
   }
 }
+
